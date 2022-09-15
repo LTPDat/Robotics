f5211363e8be044798e503f31a13d451f199437c
Lê Đạt <113450811+LTPDat@users.noreply.github.com>
2022-09-15 17:01:08 +0700

    Delete Rot_y.m

--- Homework 2/Rot_y.m
+++
@@ -1,6 +0,0 @@
-function y = Rot_y(tt)
-y = [cos_rad(tt)    0   sin_rad(tt) 0
-     0              1   0           0
-     -sin_rad(tt)   0   cos_rad(tt) 0
-     0              0   0           1];
-end
