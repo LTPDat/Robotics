809fc57b1fb325781f7d1c33e3ae0ef1c8c337b2
Lê Đạt <113450811+LTPDat@users.noreply.github.com>
2022-09-15 17:01:14 +0700

    Delete Rot_z.m

--- Homework 2/Rot_z.m
+++
@@ -1,6 +0,0 @@
-function y = Rot_z(tt)
-y = [cos_rad(tt)    -sin_rad(tt)    0   0
-     sin_rad(tt)    cos_rad(tt)     0   0
-     0              0               1   0
-     0              0               0   1];
-end
